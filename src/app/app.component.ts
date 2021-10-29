32aec5b98c4256b37534f89b2bf7788761323f2e
Enzo de Carvalho Santos <enzocsantos18@gmail.com>
2021-10-29 11:00:12 -0300

    feature: Criação service Photos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { PhotoService } from './photos/photo/photo.service';
 
 @Component({
   selector: 'app-root',
@@ -11,12 +11,9 @@ export class AppComponent {
   photos: Object[] = [];
 
 
-  constructor(http: HttpClient) {
-    http
-      .get<Object[]>("http://localhost:3000/flavio/photos")
-      .subscribe(photos =>
-        this.photos = photos,
-        err => console.error(err)
-        );
+  constructor(photoService: PhotoService) {
+    photoService
+      .listFromUser("flavio")
+      .subscribe(photos => this.photos = photos);
   }
 }
