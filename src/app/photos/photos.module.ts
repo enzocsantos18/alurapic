1a8ef4f9c46b9d8fc03e6e76cf83c3bd0bfab1b7
Enzo de Carvalho Santos <enzocsantos18@gmail.com>
2021-10-29 13:21:48 -0300

    feature: Adição paginação projeto

--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -1,13 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 
+import { PhotoFormComponent } from './photo-form/photo-form.component';
 import { PhotoListComponent } from './photo-list/photo-list.component';
 import { PhotoComponent } from './photo/photo.component';
 
 @NgModule({
-  declarations: [PhotoComponent, PhotoListComponent],
+  declarations: [PhotoComponent, PhotoListComponent, PhotoFormComponent],
   exports: [PhotoListComponent],
-  imports: [HttpClientModule]
+  imports: [HttpClientModule, CommonModule]
 })
 export class PhotosModule{
 
