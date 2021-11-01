6562dd1c918242cf1894579af4a7cdc2e6452f15
Enzo de Carvalho Santos <enzocsantos18@gmail.com>
2021-10-31 21:18:13 -0300

    feature: Implementação filter usando pipe e uso do ngIF

---
+++ src/app/photos/photo-list/filter-by-description.pipe.ts
@@ -0,0 +1,22 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+import { Photo } from './../photo/photo';
+
+@Pipe({
+  name: "filterByDescription"
+})
+export class FilterByDescription implements PipeTransform {
+  transform(photos: Photo[], filter: string) : Photo[] {
+    filter = filter.trim().toLowerCase();
+
+    if(filter) {
+      return photos.filter(photo =>
+        photo.description.toLowerCase().includes(filter)
+        )
+    }
+    else {
+      return photos;
+    }
+  }
+
+}
--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -3,12 +3,13 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 
 import { PhotoFormComponent } from './photo-form/photo-form.component';
+import { FilterByDescription } from './photo-list/filter-by-description.pipe';
 import { PhotoListComponent } from './photo-list/photo-list.component';
-import { PhotoComponent } from './photo/photo.component';
 import { PhotosComponent } from './photo-list/photos/photos.component';
+import { PhotoComponent } from './photo/photo.component';
 
 @NgModule({
-  declarations: [PhotoComponent, PhotoListComponent, PhotoFormComponent, PhotosComponent],
+  declarations: [PhotoComponent, PhotoListComponent, PhotoFormComponent, PhotosComponent, FilterByDescription],
   exports: [PhotoListComponent],
   imports: [HttpClientModule, CommonModule]
 })
